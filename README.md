af33f8835f48cb0cdc94ceec79f699945f24c5a2
Lucélia Batista <lucelia.ti@gmail.com>
2022-03-30 16:14:16 -0300

    Layout e Lógica de um urna eletrônica, criada

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Urna Eletrônica</h1>
+
+<h4 align="center"> 
+	🚧  🚀Concluído  🚧
+</h4>
+
+## Descrição do Projeto
+
+<p align="center">Réplica de layout e funcionamento lógico de uma urna eletrônica, desenvolvido para aprofundar conhecimentos sobre Javascript</p>
+
+
+---
+
+ <h2>Feito com ❤️ por Lucélia Batista 👏</h2>
+  <img style="heigth: 200px; width: 200px" src="https://avatars.githubusercontent.com/u/86496770?v=4">
+ <a href="https://www.linkedin.com/in/luceliabatista/">🚀</a>
+
+Contato:
+
+[![Linkedin Badge](https://img.shields.io/badge/-Lucelia-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/luceliabatista/)](https://www.linkedin.com/in/luceliabatista/)
+[![Gmail Badge](https://img.shields.io/badge/-lucelia.dev@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:lucelia.dev@gmail.com)](mailto:lucelia.dev@gmail.com)
---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR(A)',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulana de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '03_2.jpg', legenda: 'Vereadora', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '03.jpg', legenda: 'Vereadora', small: false }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO(A)',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclana',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '01.jpg', legenda: 'Prefeita', small: false },
+                    { url: '01_2.jpg', legenda: 'Vice-Prefeita', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '04.jpg', legenda: 'Prefeito', small: false },
+                    { url: '02_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ]
+    }
+];
